a647210278406e41c839481c4e64494f4c4c943b
Lane Buckingham <lanebuckingham@gmail.com>
2013-03-18 09:51:12 -0400

    test 4

--- change_name.sh
+++ change_name.sh
@@ -5,6 +5,13 @@ if [[ $a == "Y" || $a == "y" ]]; then
 
   echo -e "\n  The following questions are CASE-SENSITIVE: \n"
 
+  # In the following code, here is how the find command breaks down:
+  # find .         # find all files in all dirs from this one down
+  # -path "./.git" -prune -o     # but exclude the entire .git directory
+  # -type f        # and then include only files (not dirs or links)
+  # -exec sed...   # and then for every file in that list, 
+                   # execute sed to rename text in the file
+
   read -p "What is the new name for: MaryKay? " name1
   find . -path "./.git" -prune -o -type f -exec sed -i '' s/MaryKay/"$name1"/g {} +
 
