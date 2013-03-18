a2c1a7c624b7e0ab39d87cb11233a927ef0446f6
Lane Buckingham <lanebuckingham@gmail.com>
2013-03-18 09:33:53 -0400

    test 1

--- change_name.sh
+++ change_name.sh
@@ -6,15 +6,15 @@ if [[ $a == "Y" || $a == "y" ]]; then
   echo -e "\n  The following questions are CASE-SENSITIVE: \n"
 
   read -p "What is the new name for: NewAuth? " name1
-  find . -type f -exec sed -i '' s/NewAuth/"$name1"/g {} +
+  find . -path "./.git" -prune -o -type f -exec sed -i '' s/NewAuth/"$name1"/g {} +
 
   read -p "What is the new name for: newauth? " name2
-  find . -type f -exec sed -i '' s/newauth/"$name2"/g {} +
+  find . -path "./.git" -prune -o -type f -exec sed -i '' s/newauth/"$name2"/g {} +
 
   read -p "What is the new name for: Newauth? " name3
-  find . -type f -exec sed -i '' s/Newauth/"$name3"/g {} +
+  find . -path "./.git" -prune -o -type f -exec sed -i '' s/Newauth/"$name3"/g {} +
 
-  echo -e "Done! \n"
+  echo "Done!"
 else
   echo "Good-bye"
 fi
